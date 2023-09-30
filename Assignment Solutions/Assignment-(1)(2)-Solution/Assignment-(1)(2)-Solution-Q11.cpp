b6f0f1a58dffe5b9acde0defa3aa3d104ee10339
Tashfeen Abbasi <144563726+tashi-2004@users.noreply.github.com>
2023-09-30 23:25:54 +0500

    assignment solutions

---
+++ Assignment Solutions/Assignment-(1)(2)-Solution/Assignment-(1)(2)-Solution-Q11.cpp
@@ -0,0 +1,35 @@
+// Question11_22I-1855.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//LAIBA MAZHAR
+// 22I-1855
+//QUESTION11
+
+#include <iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	cout << setw(3) << "Hey." << setw(3) << "I" << setw(3) << "am" << setw(6) << "Miss" << setw(8) << "Laiba " << setw(7) << "Mazhar" << endl;
+	cout << setw(2) << "MY" << setw(5) << "Roll" << setw(8) << "number" << setw(4) << "is:" << setw(11) << "22I - 1855." << endl;
+	cout << setw(2) << "MY" << setw(7) << "Degree" << setw(3) << "is" << setw(8) << "BS(DS)" << endl;
+	cout << setw(1) << "I" << setw(5) << "Love" << setw(10) << "Pakistan:" << endl;
+		cout << setw(15) << "_______" << endl;
+		cout << setw(8) << "/" << setw(8) << "\\" << endl;
+		cout << setw(8) << "|" << setw(3) << "o" << setw(2) << "o" << setw(3) << "|" << endl;
+		cout << setw(8) << "|" << setw(4) << "^" << setw(4) << "|" << endl;
+		cout << setw(8) << "|" << setw(2) << " \\___/" << setw(2) << "|" << endl;
+		cout << setw(16) << "\\_______/" << endl;
+		return 0;
+	
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ Assignment Solutions/Assignment-(1)(2)-Solution/Assignment-(1)(2)-Solution-Q12.cpp
@@ -0,0 +1,97 @@
+// Question12_22I-1855.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// LAIBA MAZHAR
+//22I-1855
+//QUESTION # 12
+#include<iostream>
+#include<iomanip>
+    using namespace std;
+    int main()
+    {
+        cout << setw(3) << "Hey." << setw(3) << "I" << setw(3) << "am" << setw(6) << "Miss" << setw(8) << "Laiba " << setw(7) << "Mazhar" << endl;
+        cout << setw(2) << "MY" << setw(5) << "Roll"<<setw(8)<<"number" << setw(4) << "is:" << setw(11) << "22I - 1855." << endl;
+        cout << setw(2) << "MY" << setw(7) << "Degree" << setw(3) << "is" <<setw(8) <<"BS(DS)" << endl;
+        cout << setw(1) << "I" << setw(5) << "Love" << setw(10) << "Pakistan:" << endl;
+
+        cout << setw(4) << ".-." << setw(9) << "(   )" << setw(71) << "(    )" << setw(8) << "_____" << setw(8) << ".-." <<
+            setw(21) << "(" << setw(5) << ")" << endl;
+
+
+        cout << "(___)" << setw(7) << "| |" << setw(12) << ".--." << setw(7) << "___" << setw(5) << "___" << setw(8) << ".--." << setw(15) << ".---."
+            << setw(13) << ".----." << setw(8) << "|" << setw(3) << "|" << setw(4) << "(" << setw(6) << ")" << setw(8) << "(___)" <<
+            setw(13) << ".---." << setw(8) << "|" << setw(4) << "|_" << setw(15) << ".----." << setw(9) << "____" << setw(5) << ".-." << endl;
+
+
+        cout << "(''')" << setw(7) << "| |" << setw(8) << "/" << setw(5) << "\\" << setw(12) << "(   )(   )" << setw(3) << "/" << setw(5) << "\\" <<
+            setw(9) << "/" << setw(6) << "\\" << setw(6) << "/" << setw(6) << ".--," << setw(2) << "\\" << setw(6) << "|" << setw(3) << "|" <<
+            setw(5) << "'" << setw(3) << "/" << setw(10) << "(''')" << setw(8) << "/" << setw(6) << "\\" << setw(6) << "(" << setw(6) << "  __)"
+            << setw(7) << "/" << setw(6) << ".--," << setw(2) << "\\" << setw(9) << "(    )" << setw(6) << "\\" << endl;
+
+
+        cout << setw(4) << "| |" << setw(8) << "| |" << setw(14) << "|  .-. ;" << setw(5) << "| |" << setw(5) << "| |" <<
+            setw(3) << "|" << setw(6) << ".-." << setw(2) << ";" << setw(7) << "'" << setw(4) << "._," << setw(3) << ";" << setw(8) << "(___)"
+            << setw(3) << ";" << setw(2) << "|" << setw(6) << "|" << setw(3) << "|" << setw(4) << "." << setw(3) << "/" << setw(8) << "|" << setw(2) << "|"
+            << setw(8) << "." << setw(5) << ":-." << setw(3) << ";" << setw(6) << "|" << setw(3) << "|" << setw(12) << "(___)"
+            << setw(3) << ";" << setw(2) << "|" << setw(5) << "|" << setw(6) << ".-." << setw(3) << "." << endl;
+
+
+        cout << setw(4) << "| |" << setw(8) << "| |" << setw(9) << "| |" << setw(5) << "| |" << setw(5) << "| |" << setw(5) << "| |" << setw(3)
+            << "|" << setw(3) << "|" << setw(2) << "|" << setw(2) << "|" << setw(7) << "|" << setw(3) << "|" << setw(3) << "." << setw(2) << "|"
+            << setw(10) << ".-'" << setw(3) << "|" << setw(6) << "|" << setw(3) << "|" << setw(2) << "," << setw(3) << "." << setw(10) << "|" << setw(2) << "|" <<
+            setw(8) << "|" << setw(3) << "." << setw(4) << "|" << setw(3) << "|" << setw(4) << "|" << setw(3) << "|" << setw(4) << "___"
+            << setw(8) << ".'" << setw(5) << "|" << setw(5) << "|" << setw(3) << "|" << setw(5) << "|" << setw(3) << "|" << endl;
+
+
+        cout << setw(4) << "| |" << setw(8) << "| |" << setw(9) << "| |" << setw(5) << "| |" << setw(5) << "| |" << setw(5)
+            << "| |" << setw(3) << "|" << setw(3) << "|" << setw(1) << "/" << setw(3) << "|" << setw(7) << "|" << setw(3) << "|" << setw(3)
+            << "|" << setw(2) << "|" << setw(6) << "/" << setw(3) << ".'" << setw(1) << "|" << setw(3) << "|" << setw(6) << "." << setw(3) << "'" <<
+            setw(4) << "'" << setw(11) << "|" << setw(2) << "|" << setw(13) << "\\___'." << setw(5) << "(___)" << setw(4) << "|" << setw(3) << "|" << "(   )"
+            << setw(5) << "/" << setw(3) << ".'" << setw(1) << "|" << setw(3) << "|" << setw(5) << "|" << setw(3) << "|" << setw(5) << "|" << setw(3) << "|" << endl;
+
+
+        cout << setw(4) << "| |" << setw(8) << "| |" << setw(9) << "| |" << setw(5) << "| |" << setw(5) << "| |" << setw(5) << "| |" <<
+            setw(3) << "|" << setw(3) << "'" << setw(2) << ".-'" << setw(8) << "|" << setw(3) << "|" << setw(3) << "|" << setw(2) << "|" <<
+            setw(5) << "|" << setw(2) << "/" << setw(3) << "|" << setw(3) << "|" << setw(6) << "|" << setw(3) << "|"
+            << setw(3) << "." << setw(3) << "'" << setw(9) << "|" << setw(2) << "|" << setw(8) << "(" << setw(4) << ")" << "." << setw(3) << "." <<
+            setw(6) << "|" << setw(3) << "|" << setw(2) << "|" << setw(2) << "|" <<
+            setw(5) << "|" << setw(2) << "/" << setw(3) << "|" << setw(3) << "|" << setw(5) << "|" << setw(3) << "|" << setw(5) << "|" << setw(3) << "|" << endl;
+
+
+        cout << setw(4) << "| |" << setw(8) << "| |" << setw(9) << "| '" << setw(5) << "| |" << setw(5) << "' '" << setw(5) << "; ." <<
+            setw(3) << "|" << setw(7) << ":_:-." << setw(7) << "|" << setw(3) << "|" << setw(3) << "'" << setw(2) << "|" << setw(5) << ";" <<
+            setw(2) << "|" << setw(3) << ";" << setw(3) << "|" << setw(6) << "|" << setw(3) << "|" << setw(4) << "\\" << setw(3) << "\\" <<
+            setw(8) << "|" << setw(2) << "|" << setw(9) << "|" << setw(2) << "|" << setw(4) << "\\" << setw(2) << "!" <<
+            setw(5) << "|" << setw(3) << "'" << setw(2) << "|" << setw(2) << "|" <<
+            setw(5) << ";" << setw(2) << "|" << setw(3) << ";" << setw(3) << "|" << setw(5) << "|" << setw(3) << "|" << setw(5) << "|" << setw(3) << "|" << endl;
+
+
+        cout << setw(4) << "| |" << setw(8) << "| |" << setw(14) << ".  ._. /" << setw(4) << "\\" << setw(2) << "." << setw(1) << "." <<
+            setw(2) << "/" << setw(4) << "'" << setw(7) << "/" << setw(7) << "|" << setw(5) << "._." << setw(2) << "." << setw(10) << "._." <<
+            setw(4) << "|" << setw(6) << "|" << setw(3) << "|" << setw(5) << "\\" << setw(3) << "\\" << setw(7) << "|" << setw(2) << "|" <<
+            setw(9) << ";" << setw(6) << "'._,'" << setw(2) << "." << setw(10) << "'  ._." << setw(2) << ";" <<
+            setw(5) << "." << setw(5) << "._." << setw(3) << "|" << setw(5) << "|" << setw(3) << "|" << setw(5) << "|" << setw(3) << "|" << endl;
+
+
+        cout << "(___)" << setw(8) << "(___)" << setw(12) << "'.__.'" << setw(9) << "._.'" << setw(11) << "'.__.'" <<
+            setw(8) << "|" << setw(7) << "\\__.'" << setw(11) << "'.___." << "'_." << setw(10) << "(____)" << setw(9) << "(____)" <<
+            setw(8) << "(___)" << setw(15) << "'.___.'" << setw(13) << "'.___.'" <<
+            setw(11) << "'.___." << "'_." << setw(8) << "(____)" << setw(8) << "(____)" << endl;
+
+        cout << setw(53) << "|" << setw(3) << "|" << endl;
+
+
+        cout << setw(57) << "(____)" << endl;
+
+
+        return 0;
+    }
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
